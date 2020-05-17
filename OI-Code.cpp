4cad2bfeb5dd1434effc1b07bbcba54aa47d6b8d
墨宇 <MoyuSteve@outlook.com>
2020-05-17 22:45:01 +0800

    过河 https://www.nowcoder.com/study/live/249/11/4

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,30 +1,53 @@
-#include <iostream>
-#include <algorithm>
-#include <cstring>
+#define _CRT_SECURE_NO_WARNINGS // 谔谔
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
 using namespace std;
-const int maxn = 2555;
-int a[maxn], dp[maxn], sum[maxn];
-int main()
+
+const int maxm = 110,inf = 1e9+7;
+
+
+//先考虑dp.
+int stones[maxm], w[maxm*maxm],f[maxm * maxm];//[maxm * maxm]:小小优化，石头之间隔>100没有意义，缩短！
+
+int S, T, m;
+
+int main(void)
 {
-	char c;
-	int n, k, x, y;
-	cin >> n >> k;
-	memset(a, 0, sizeof(a));
-	dp[0] = sum[0] = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> c;
-		sum[i] = sum[i - 1] + (c == 'H');//H队
-		dp[i] = i;
-	}
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= i; j++)
-		{
-			x = sum[i] - sum[j - 1];
-			y = i - j - x + 1;
-			if (y == 0 || x == 0 || abs(x - y) <= k)
-				dp[i] = min(dp[i], dp[j - 1] + 1);
-		}
-	cout << dp[n] << endl;
+    scanf("%*d%d%d%d", &S, &T, &m);
+    for (int i = 0; i < m; i++)scanf("%d", stones + i);
+    if (S == T)
+    {
+        int res = 0;
+        for (int i = 0; i < m; i++)
+            if (stones[i] % S == 0)res++;
+        printf("%d", res); 
+        return 0;
+    }
+    sort(stones, stones + m);
+    int last = 0, k = 0;
+
+    //构造图
+    for (int i = 0; i < m; i++)
+    {
+        //stones[i] - last = 100 => last = stones[i] - 100;
+        if(stones[i] - last > 100)last = stones[i] - 100;
+        for (int j = 0; j < stones[i] - last; j++)w[++k] = 0;
+        w[k] = 1;
+        last = stones[i];
+    }
+    //开始dp
+    for (int i = 1; i <= k + 10; i++)
+    {
+        f[i] = inf;
+        for (int j = S; j <= T; j++)
+            if (i - j >= 0)
+                f[i] = min(f[i], f[i - j] + w[i]);
+    }
+
+    int res = inf;
+    for (int i = k; i <= k + 10; i++) res = min(res, f[i]);
+    printf("%d\n", res);
 	return 0;
 }
