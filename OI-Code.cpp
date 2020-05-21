a4c69509082ac68f65fc75c04d2511d5a45e7e8e
墨宇 <MoyuSteve@outlook.com>
2020-05-21 21:53:48 +0800

    比例简化

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,26 +1,19 @@
-//扫雷游戏
+//比例简化
 #include <cstdio>
 #include <iostream>
+// 现给出支持人数A，反对人数 B ，以及一个上限 L ，请你将 A 比 B 化简为 A ’比 B ’，
+// 要求在 A ’和 B ’均不大于 L 且 A ’和 B ’互质（两个整数的最大公约数是 1 ）的前提下，
+// A ’ /B ’ ≥ A/B 且 A ’ /B ’ - A/B 的值尽可能小。
+
 using namespace std;
-const int maxn = 110;
-int n,m;
-char a[maxn][maxn];
-int cnt[maxn][maxn];
-const int dx[8] = {-1,0,1,-1,1,-1,0,1};
-const int dy[8] = {-1,-1,-1,0,0,1,1,1};
-int main()
+int a, b, c, aa, bb;
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int main(void)
 {
-    scanf("%d%d",&n,&m);
-    for(int i = 1;i <= n;i++)
-        for(int j = 1;j <= m;j++)
-            cin>>a[i][j];
-    for(int i = 1;i <= n;i++)
-        for(int j = 1;j <= m;j++)
-            if(a[i][j] == '*')
-                for(int k = 0;k < 8;k++)
-                    cnt[i+dx[k]][j+dy[k]]++;
-    for(int i = 1;i <= n;i++,puts(""))
-        for(int j = 1;j <= m;j++)
-            if(a[i][j] == '?')cout<<cnt[i][j];
-            else cout<<a[i][j];
+    cin >> a >> b >> c;
+    for (int i = 0; i <= c; i++)
+        for (int j = 1; j <= c; j++)
+            if (gcd(i, j) == 1 && ((double)i / j) >= ((double)a / b) && ((aa == 0 && bb == 0) || ((((double)i/j) - ((double)a/b)) < (((double)aa/bb) - ((double)a/b)))))
+                aa = i,bb = j;
+    cout<<aa<<" "<<bb;
 }
