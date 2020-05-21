8be8aa03c672b0b30f4d81c114a2c5eea755a18e
墨宇 <MoyuSteve@outlook.com>
2020-05-21 21:59:51 +0800

    回文日期

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,19 +1,22 @@
-//比例简化
-#include <cstdio>
+//回文日期
 #include <iostream>
-// 现给出支持人数A，反对人数 B ，以及一个上限 L ，请你将 A 比 B 化简为 A ’比 B ’，
-// 要求在 A ’和 B ’均不大于 L 且 A ’和 B ’互质（两个整数的最大公约数是 1 ）的前提下，
-// A ’ /B ’ ≥ A/B 且 A ’ /B ’ - A/B 的值尽可能小。
-
+#include <algorithm>
+#include <cstdio>
 using namespace std;
-int a, b, c, aa, bb;
-int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
-int main(void)
+int ts[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, b, e, ans = 0;
+int main()
 {
-    cin >> a >> b >> c;
-    for (int i = 0; i <= c; i++)
-        for (int j = 1; j <= c; j++)
-            if (gcd(i, j) == 1 && ((double)i / j) >= ((double)a / b) && ((aa == 0 && bb == 0) || ((((double)i/j) - ((double)a/b)) < (((double)aa/bb) - ((double)a/b)))))
-                aa = i,bb = j;
-    cout<<aa<<" "<<bb;
+    cin >> b >> e;
+    for (int i = 1; i <= 12; i++)
+    {
+        for (int j = 1; j <= ts[i]; j++)
+        {
+            int A = j % 10, B = j / 10, C = i % 10, D = i / 10;
+            int date = A + B * 10 + C * 100 + D * 1000 + D * 10000 + C * 100000 + B * 1000000 + A * 10000000;
+            if (date >= b && date <= e)
+                ans++;
+        }
+    }
+    cout << ans;
+    return 0;
 }
