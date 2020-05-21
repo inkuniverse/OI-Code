63de7587026c92fa202061824e6bfca471105e6b
墨宇 <MoyuSteve@outlook.com>
2020-05-21 21:35:43 +0800

    扫雷游戏

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,43 +1,26 @@
-//金明的预算方案
-#include<iostream>
-#include<algorithm>
-#include<cstdio>
-#include<cstring>
-#include<vector>
-
-const int maxn = 32010,maxm = 70;
+//扫雷游戏
+#include <cstdio>
+#include <iostream>
 using namespace std;
-int n,m,v,p,q,f[maxn];
-pair<int,int> a[maxm];
-vector<pair<int,int> > G[maxm];
-int main(void)
+const int maxn = 110;
+int n,m;
+char a[maxn][maxn];
+int cnt[maxn][maxn];
+const int dx[8] = {-1,0,1,-1,1,-1,0,1};
+const int dy[8] = {-1,-1,-1,0,0,1,1,1};
+int main()
 {
-    cin >> m >> n;
-    for(int i = 1; i <= n;i++)
-    {
-        scanf("%d%d%d",&v,&p,&q);
-        p *= v;
-        if(q)G[q].push_back({v,p});
-        else a[i] = {v,p};
-    }
-    
-    for (int i = 1; i <= n; i ++ )
-        for (int j = m; j >= 0; j--)
-        {
-            if (!a[i].first) continue;
-            for (int k = 0; k < 1 << G[i].size(); k++)//神奇的二进制枚举
-            {
-                int v = a[i].first, w = a[i].second;
-                for (int u = 0; u < G[i].size(); u++)
-                    if (k >> u & 1)
-                    {
-                        v += G[i][u].first;
-                        w += G[i][u].second;
-                    }
-                if (j >= v) f[j] = max(f[j], f[j - v] + w);
-            }
-        }
- 
-    printf("%d\n", f[m]);
-    return 0;
+    scanf("%d%d",&n,&m);
+    for(int i = 1;i <= n;i++)
+        for(int j = 1;j <= m;j++)
+            cin>>a[i][j];
+    for(int i = 1;i <= n;i++)
+        for(int j = 1;j <= m;j++)
+            if(a[i][j] == '*')
+                for(int k = 0;k < 8;k++)
+                    cnt[i+dx[k]][j+dy[k]]++;
+    for(int i = 1;i <= n;i++,puts(""))
+        for(int j = 1;j <= m;j++)
+            if(a[i][j] == '?')cout<<cnt[i][j];
+            else cout<<a[i][j];
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # New to us? Try clone!
 
 `git clone https://github.com/inkuniverse/OI-code`
-Than install nginx (or caddy),become nginx(or caddy)'s root to OI-code\RemoteWorking.
+
