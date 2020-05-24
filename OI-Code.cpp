84b2408835411bcae103d35ab7793a359b327298
Steve Inkuniverse Ling <inkuniverse@outlook.com>
2020-05-24 09:12:06 +0800

    字串

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,45 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
-const int maxn = 81;
-
-inline int read() 
+#include<iostream>
+using namespace std;const int maxn = 233,maxm = 1010;
+long long f[maxn][maxn],sum[maxn][maxn],n,m,K;
+char a[maxm],b[maxn];
+int main(void)
 {
-    int X=0,w=1;
-    char ch=getchar();
-    while(ch<'0' || ch>'9') {if(ch=='-') w=-1;ch=getchar();}
-    while(ch>='0' && ch<='9') X=(X<<3)+(X<<1)+ch-'0',ch=getchar();
-    return X*w;
-}
-
-int n,m;
-int num[maxn];
-__int128 ans,p[maxn],f[maxn][maxn];
-
-__int128 dp(int L,int R)
-{
-    if(f[L][R]!=-1) return f[L][R];
-    if(R-L>=1) f[L][R]=max(num[L]*p[m-R+L]+dp(L+1,R),dp(L,R-1)+num[R]*p[m-R+L]);
-    else f[L][R]=num[L]*p[m-R+L];
-    return f[L][R];
-}
-
-void print(__int128 x)
-{
-    if(x > 9) print(x/10);
-    putchar(x%10+'0');
-}
-
-int main()
-{
-    n = read();m = read();
-    p[0]=1;
-    for(int i=1;i<=m;i++) p[i]=p[i-1]*2;
+    f[0][0] = 1;cin>>n>>m>>K>>a>>b;
     for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=m;j++) num[j] = read();
-        memset(f,-1,sizeof(f));
-        ans+=dp(1,m);
-    }
-    print(ans);
-    return 0;
+      for(int j=m;j>=1;j--)
+        for(int k=K;k>=1;k--)
+              f[j][k]=(f[j][k]+ (sum[j][k]= a[i-1]==b[j-1]? sum[j-1][k]+f[j-1][k-1] : 0))%1000000007;
+            //   f[j][k]=(f[j][k]+ (sum[j][k]= (a[i-1]==b[j-1])? (sum[j-1][k]+f[j-1][k-1]) : 0))%1000000007;
+    cout<<f[m][K];
 }
