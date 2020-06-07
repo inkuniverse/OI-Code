d2e6301651f070cf4e5179e962da68463a4e31ef
inkuniverse <inkuniverse@outlook.com>
2020-06-07 18:25:00 +0800

    数字游戏

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,34 +1,51 @@
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <algorithm>
-#define x first
-#define y second
+#include<iostream>
+#include<algorithm>
 
 using namespace std;
-const int N = 100010;
+const int maxn = 110,maxm=11,inf=1e9+7;
+int n,m;
+int a[maxn],f[maxn][maxn][maxm],g[maxn][maxn][maxm];
 
-pair<int, int> w[N];
-int n,f[N],g[N],h[N];
-/*
-f[i] = A[1] + ... + A[i]
-g[i] = max(S[1], S[2], ..., S[i])
-h[i] = max(A[i] + 2S[i], A[i + 1] + 2S[i + 1], ..., A[n] + 2S[n])
-*/
+//int sum(int l,int r)
+//{
+//	return ((a[r]-a[l-1])%10+10)%10;
+//}
+#define sum(l,r) (((a[r]-a[l-1])%10+10)%10)
 
-int main()
+
+int main(void)
 {
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) scanf("%d", &w[i].y);
-    for (int i = 1; i <= n; i++) scanf("%d", &w[i].x);
-    sort(w + 1, w + n + 1);
-    reverse(w + 1, w + n + 1);
-
-    for (int i = 1; i <= n; i++) f[i] = f[i - 1] + w[i].x;
-    for (int i = 1; i <= n; i++) g[i] = max(g[i - 1], w[i].y);
-    for (int i = n; i; i--) h[i] = max(h[i + 1], w[i].x + 2 * w[i].y);
-
-    for (int x = 1; x <= n; x++) printf("%d\n", max(f[x - 1] + h[x], f[x] + 2 * g[x]));
-
-    return 0;
+	scanf("%d%d",&n,&m);
+	for(int i = 1;i <= n;i++)
+	{
+		scanf("%d",a + i);a[i+n] = a[i];
+	}
+	for(int i = 1;i <= n*2;i++)a[i] += a[i-1];//前缀和板子
+	for(int i = 1;i <= m;i++)
+		for(int len = 1;len <= n;len++)
+			for(int l = 1;l <= n;l++)
+			{
+				int r = l+len-1;//区间长度板子
+				if(i == 1)f[l][r][i] = g[l][r][i] = sum(l,r);
+				else
+				{
+					f[l][r][i] = -inf;
+					g[l][r][i] = -f[l][r][i];
+					for(int k = l+i-1;k <= r;k++)//区间长度板子
+					{
+						int t = sum(k,r);
+						f[l][r][i] = max(f[l][r][i],f[l][k-1][i-1]*t);
+						g[l][r][i] = min(g[l][r][i],g[l][k-1][i-1]*t);
+					}
+				}
+			}
+	//min_element
+	int minv = inf,maxv = -inf;
+	for(int i = 1;i <= n;i++)
+	{
+		minv = min(minv,g[i][i+n-1][m]);
+		maxv = max(maxv,f[i][i+n-1][m]);
+	}
+	cout<<minv<<endl<<maxv<<endl;
+	return 0;
 }
