a94308e490ea5b3e4af14b06b014e9d9efefbac6
inkuniverse <inkuniverse@outlook.com>
2020-06-19 21:01:45 +0800

    表达式求值

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,33 +1,56 @@
-#include<iostream>
-#include<algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
 
 using namespace std;
-const int maxn = 300010;
-int m,s,t;
-int f[maxn];
-int main(void)
+
+const int N = 100010, mod = 10007;
+
+stack<char> sOper;
+stack<vector<int>> sNumber;
+
+void eval()
 {
-	scanf("%d%d%d",&m,&s,&t);
-	//闪现
-	for(int i = 1;i <= t;i++)
-	{
-		if(m >= 10)
-			f[i] = f[i-1] + 60,m -= 10;
-		else
-			m += 4,f[i] = f[i-1];
-	}
-	//跑步
-	for(int i = 1;i <= t;i++)
-		f[i] = max(f[i],f[i-1] + 17);
-	if (f[t] >= s)
-	{
-		for (int i = 1; i <= t; i++)
-			if (f[i] >= s)
-			{
-				printf("Yes\n%d\n", i);
-				break;
-			}
-	}
-	else printf("No\n%d\n", f[t]);
-	return 0;
+    auto c = sOper.top(); sOper.pop();
+    auto a = sNumber.top(); sNumber.pop();
+    auto b = sNumber.top(); sNumber.pop();
+
+    if (c == '+') sNumber.push({ a[0] * b[0] % mod, (a[0] * b[1] + a[1] * b[0] + a[1] * b[1]) % mod });
+    else sNumber.push({(a[0] * b[0] + a[0] * b[1] + a[1] * b[0]) % mod, a[1] * b[1] % mod});
+}
+
+int main()
+{
+    int n;
+    string expr;
+    cin >> n >> expr;
+
+    map<char, int> pr;
+    pr['('] = 0, pr['+'] = 1, pr['*'] = 2;//优先级
+
+    sNumber.push({ 1, 1 });
+    for (auto c : expr)
+    {
+        if (c == '(') sOper.push(c);
+        else if (c == '+' || c == '*')
+        {
+            while (!sOper.empty() && pr[sOper.top()] >= pr[c]) eval();
+            sNumber.push({ 1, 1 });
+            sOper.push(c);
+        }
+        else
+        {
+            while (sOper.top() != '(') eval();
+            sOper.pop();
+        }
+    }
+
+    while (!sOper.empty()) eval();//继续操作直到空
+    cout << sNumber.top()[0] << endl;
+
+    return 0;
 }
