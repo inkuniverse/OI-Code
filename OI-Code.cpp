5c37ff149fcb8aa465ac8f141e0418313763a2e2
inkuniverse <inkuniverse@outlook.com>
2020-07-13 17:05:22 +0800

    nxd

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,31 +1,24 @@
-#include <cstdio>
-#include <algorithm>
-const int maxn = 10010;
-int n, a[maxn], k;
-int T[maxn],cnt;
-void merge_sort(int *A,int x,int y,int *T,int &cnt)
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int maxn = 100010;
+int n,a[maxn];
+void qq(int l,int r)
 {
-	if(y-x>1)
+	int i = l,j = r,m = i+j>>1;
+	do
 	{
-		int m = x+(y-x)/2;
-		int p = x,q = m,i = x;
-		merge_sort(A,x,m,T,cnt);
-		merge_sort(A,m,y,T,cnt);
-                //此时左半右半都有序
-		while(p<m || q<y)
-		{
-			if(q >= y || (p<m && A[p]<=A[q])) T[i++] = A[p++];
-			else {T[i++] = A[q++];cnt += m - p; }
-		}
-		for(int i = x;i < y;i++)A[i] = T[i];
-	}
+		while(a[i] < m)i++;while(a[j] > m)j--;
+		if(i<=j)swap(a[i++],a[j--]);
+	} while (i <= j);
+	if(i < r)qq(i,r);if(l < j)qq(l,j);
 }
 int main(void)
 {
-	int n;
-	scanf("%d",&n);
+	cin >> n;
 	for(int i = 0;i < n;i++)
 		scanf("%d",a+i);
-	merge_sort(a,0,n,T,cnt);
-	printf("%d",cnt);
+	sort(a,a+n);
+	for(int i = 0;i < n;i++)
+		printf("%d ",a[i]);
 }
