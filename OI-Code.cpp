87b65ea7e5c224c158e8ebb4d219a94e5a608feb
inkuniverse <inkuniverse@outlook.com>
2020-07-13 18:28:46 +0800

    Update OI-Code.cpp

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,24 +1,10 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
 using namespace std;
-const int maxn = 100010;
-int n,a[maxn];
-void qq(int l,int r)
-{
-	int i = l,j = r,m = i+j>>1;
-	do
-	{
-		while(a[i] < m)i++;while(a[j] > m)j--;
-		if(i<=j)swap(a[i++],a[j--]);
-	} while (i <= j);
-	if(i < r)qq(i,r);if(l < j)qq(l,j);
-}
+int a[]={1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,
+	28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,
+	24157817,39088169,63245986,102334155,165580141,267914296,433494437,701408733,1134903170,1836311903};
 int main(void)
 {
-	cin >> n;
-	for(int i = 0;i < n;i++)
-		scanf("%d",a+i);
-	sort(a,a+n);
-	for(int i = 0;i < n;i++)
-		printf("%d ",a[i]);
+	int n;cin>>n;
+	cout<<a[n];
 }
