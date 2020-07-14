fa9b32ade54695fd1b64c65085d06ddfe2c9b36f
inkuniverse <inkuniverse@outlook.com>
2020-07-14 16:19:26 +0800

    p1330 100pts

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,83 +1,61 @@
-
-#include<iostream>
 #include<cstdio>
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<string>
 #include<algorithm>
-#include<cstring>
-#include<set>
 using namespace std;
-
-const int maxn = 1e5+10,maxm = 1e6+10;
-int n, m;
-int head[maxn],cnt,a[maxn],vis[maxn];
-set<int> s;
 struct edge
 {
-	int to, next;
-}e[maxm];
-
-struct Initialization
+    int to;
+    int next;
+}e[200000];
+int head[20000];
+int cnt,black,white;
+void add(int a, int b)
 {
-	
-}Initializationner;
-int Impossible, black, white;
-int fa[maxn];
-
-int find(int t)						 //各个连通子图
-{									 //各个连通子图
-	if (fa[t] == t)return t;		 //各个连通子图
-	return fa[t] = find(fa[t]);		 //各个连通子图
-}									 //各个连通子图
-void Union(int x, int y)			 //各个连通子图
-{									 //各个连通子图
-	x = find(x), y = find(y);		 //各个连通子图
-	fa[x] = y;						 //各个连通子图
-}									 //各个连通子图
-void dfs(int root, int color)
+    cnt++;
+    e[cnt].to = b;
+    e[cnt].next = head[a];
+    head[a] = cnt;
+}
+bool used[20000];
+int col[20000];
+bool dfs(int node, int color)
 {
-	if (Impossible)return;
-	if (a[root])
-	{
-		Impossible = (a[root] != color);//无解
-		return;
-	}
-	a[root] = color;
-	if (color)white++; else black++;
-	for (int i = head[root]; i; i = e[i].next)
-	{
-		int v = e[i].to;
-		dfs(v, !color);
-	}
+    if (used[node])return (col[node] == color);
+    used[node] = true;
+    col[node] = color;
+    if (color) white++; else black++;
+    for (int i = head[node]; i; i = e[i].next)
+    {
+        if(!dfs(e[i].to, 1 - color))return 0;
+    }
+    return 1;
 }
 int main()
 {
-	auto add = [&](int a, int b)->void
-	{
-		e[++cnt].next = head[a];
-		e[cnt].to = b;
-		head[a] = cnt;
-	};
-	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++)fa[i] = i;
-	for (int i = 1; i <= m; i++)
-	{
-		int u, v;
-		scanf("%d%d", &u, &v);
-		add(u, v);
-		add(v, u);
-		Union(u, v);
-		vis[u] = vis[v] = 1;
-	}
-
-	for (int i = 1; i <= n; i++)if(vis[i] == 1 && fa[i] == i)s.insert(i);
-	//Solver();
-	Impossible = 0;
-	int ans = 0;
-	for (auto i : s)
-	{
-		black = white = 0;
-		dfs(i, 0);
-		ans += min(white, black);
-	}
-	if (Impossible)puts("Impossible");
-	else cout << ans << endl;
+    int n, m;
+    scanf("%d%d", &n, &m);
+    int a, b;
+    while (m--)
+    {
+        scanf("%d%d", &a, &b);
+        add(a, b);
+        add(b, a);
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (used[i])continue;
+        black = white = 0;
+        if (!dfs(i, 0))
+        {
+            printf("Impossible");
+            return 0;
+        }
+        ans += min(black, white);
+    }
+    printf("%d", ans);
+    return 0;
 }
