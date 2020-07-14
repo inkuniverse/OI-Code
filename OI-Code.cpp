cc8413bf249bbb0ac0f75a9e8e46a4e7a646eb34
inkuniverse <inkuniverse@outlook.com>
2020-07-14 16:02:46 +0800

    P1330 20p

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,62 +1,66 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<cstdio>
+
 #include<iostream>
-#include<cmath>
-#include<string>
-#include<string>
+#include<cstdio>
 #include<algorithm>
+#include<cstring>
 using namespace std;
-struct Edge
+
+const int maxn = 1e5+10,maxm = 1e6+10;
+int n, m;
+int head[maxn],cnt,a[maxn];
+struct edge
 {
-    int to;
-    int next;
-}edge[200000];
-int head[20000];
-int cnt,black,white;
-void add(int a, int b)
+	int to, next;
+}e[maxm];
+struct Initialization
 {
-    cnt++;
-    edge[cnt].to = b;
-    edge[cnt].next = head[a];
-    head[a] = cnt;
-}
-bool used[20000];
-int col[20000];
-bool dfs(int node, int color)
-{
-    if (used[node])return (col[node] == color);
-    used[node] = true;
-    col[node] = color;
-    if (color) white++; else black++;
-    for (int i = head[node]; i; i = edge[i].next)
-    {
-        if(!dfs(edge[i].to, 1 - color))return 0;
-    }
-    return 1;
-}
-int main()
-{
-    int n, m;
-    scanf("%d%d", &n, &m);
-    int a, b;
-    while (m--)
-    {
-        scanf("%d%d", &a, &b);
-        add(a, b);
-        add(b, a);
-    }
-    int ans = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        if (used[i])continue;
-        black = white = 0;
-        if (!dfs(i, 0))
-        {
-            printf("Impossible");
-            return 0;
-        }
-        ans += min(black, white);
-    }
-    printf("%d", ans);
-    return 0;
-}
+	int Impossible,black,white;
+	Initialization()
+	{
+		InputGraph();
+		Solver();
+	}
+	void InputGraph()
+	{
+		auto add = [&](int a, int b)->void
+		{
+			e[++cnt].next = head[a];
+			e[cnt].to = b;
+			head[a] = cnt;
+		};
+		scanf("%d%d", &n, &m);
+		for (int i = 0; i < m; i++)
+		{
+			int u, v;
+			scanf("%d%d", &u, &v);
+			add(u, v);
+			add(v, u);
+		}
+	}
+	void Solver()
+	{
+		Impossible = 0; 
+		black = white = 0;
+		dfs(1, 0);
+		if (Impossible)puts("Impossible");
+		else cout << min(white,black) << endl;
+	}
+	void dfs(int root,int color)
+	{
+		if (Impossible)return;
+		if(a[root])
+		{
+			Impossible = (a[root] != color);//无解
+			return;
+		}
+		a[root] = color;
+		if (color)white++; else black++;
+		for (int i = head[root]; i; i = e[i].next)
+		{
+			int v = e[i].to;
+			dfs(v, !color);
+		}
+	}
+}Initializationner;
+
+int main(){}
