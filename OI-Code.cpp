c03805b4285f01584a00ecbed02faceec87b4886
inkuniverse <inkuniverse@outlook.com>
2020-07-17 09:09:40 +0800

    horsemove

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,47 +1,32 @@
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-#include <cstdio>
-using namespace std;
-int n;
-double a[30][2],lt[30][30],zt[(1<<15)+15][18];
-double answer;
-int bj[30],i,j;//bg是辅助用于记录的/qw:
-void dfs(int y,int now,int x,long double ans)
+#include<stdio.h>
+#include<math.h>
+int ans;
+int sx,sy,tx,ty;
+int excel[5][5]=
+	{{0,3,2,3,2},
+	 {3,2,1,2,3},
+	 {2,1,4,3,2},
+	 {3,2,3,2,3},
+	 {2,3,2,3,4}};
+void swap(int *x,int *y)
 {
-	if(x==n+1&&(answer==0 || answer>ans)) 
-	{
-		answer=ans;
-		return;
-	}
-	for(int val=1;val<=n;val++)
-	{
-		if(!bj[val])
-		{
-			int xb=now+(1<<(val-1));
-			if(zt[xb][val]!=0)
-				if(zt[xb][val]<=ans+lt[y][val]) 
-					continue;
-			bj[val]=1;
-			zt[xb][val]=ans+lt[y][val];
-			dfs(val,xb,x+1,zt[xb][val]);
-			bj[val]=0;
-		}
-	}
+	int t = *x;
+	*x = *y;
+	*y = t;
 }
-
 int main()
 {
-	cin>>n;
-	a[0][0]=0;a[0][1]=0;
-	for(i=1;i<=n;i++)
-	{
-		scanf("%lf%lf",&a[i][0],&a[i][1]);
-		for(j=0;j<i;j++)
-			lt[i][j]=lt[j][i]=sqrt((a[i][0]-a[j][0])*(a[i][0]-a[j][0])+(a[i][1]-a[j][1])*(a[i][1]-a[j][1]));
-		//预处理距离
+	scanf("%d%d%d%d",&sx,&sy,&tx,&ty);
+	int x=abs(sx-tx);
+	int y=abs(sy-ty);
+	while(x>4||y>4)
+	{//贪心
+		if(x<0)x=-x;
+		if(y<0)y=-y;
+		if(x<y)swap(&x,&y);
+		x-=2;y-=1;
+		ans++;
 	}
-	dfs(0,0,1,0);
-	printf("%.2lf",answer);
+	printf("%d\n",ans+excel[x][y]);
 	return 0;
 }
