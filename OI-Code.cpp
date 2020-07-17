9bc6c929ef450b07de75051ca0553e631a9fdf75
inkuniverse <inkuniverse@outlook.com>
2020-07-17 13:24:56 +0800

    hellow

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,18 +1,3 @@
 #include<iostream>
-#include<algorithm>
-#include<cstdio>
 using namespace std;
-const int maxn = 100010;
-int a,n,cnt,m,sum;
 int main(void)
-{
-	scanf("%d%d",&n,&m);
-	for(int i = 0; i < n;i++) 
-	{
-		scanf("%d",&a);
-		if(sum + a > m){sum = a;cnt++;}
-		else {sum += a;}
-	}
-	cout<<cnt+1;//最后还有一组
-	return 0;
-}
