b88ffdeef8253d856d8ff600bd92c35a33a2f120
inkuniverse <inkuniverse@outlook.com>
2020-07-19 14:00:09 +0800

    omg

--- OI-Code.cpp
+++ OI-Code.cpp
@@ -1,32 +1,37 @@
-#include<stdio.h>
-#include<math.h>
-int ans;
-int sx,sy,tx,ty;
-int excel[5][5]=
-	{{0,3,2,3,2},
-	 {3,2,1,2,3},
-	 {2,1,4,3,2},
-	 {3,2,3,2,3},
-	 {2,3,2,3,4}};
-void swap(int *x,int *y)
-{
-	int t = *x;
-	*x = *y;
-	*y = t;
-}
-int main()
-{
-	scanf("%d%d%d%d",&sx,&sy,&tx,&ty);
-	int x=abs(sx-tx);
-	int y=abs(sy-ty);
-	while(x>4||y>4)
-	{//贪心
-		if(x<0)x=-x;
-		if(y<0)y=-y;
-		if(x<y)swap(&x,&y);
-		x-=2;y-=1;
-		ans++;
-	}
-	printf("%d\n",ans+excel[x][y]);
-	return 0;
-}
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    int str[256];
+    vector<int> G[int(1e5+10)];
+    int fa[int(1e5+10)];
+    vector<int> ans;
+    int aa[int(1e5+10)][256];
+    string label;
+public:
+    int count(int u,char color)
+    {
+        if(aa[u][color])return aa[u][color];
+        int ans = (label[u] == color);
+        for(auto v : G[u])
+            if(v != fa)
+                ans += count(v,color);
+        return aa[u][color] = ans;
+    }
+    void dfs(int u)
+    {
+        for(auto v : G[u])
+            if(v != fa[u])
+                fa[v] = u,dfs(v);
+    }
+    vector<int> countSubTrees(int n, vector<vector<int> >& edges, string labels) {
+        for(auto i : edges)
+            G[i[0]].push_back(i[1]),G[i[1]].push_back(i[0]);
+        label = labels;
+        fa[0] = -1;
+        dfs(0);
+        for(int i = 0;i < n;i++)
+            ans.push_back(count(i,label[i]));
+        return ans;
+    }
+};
+int main(){}
