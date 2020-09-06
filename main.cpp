12c69403f2f00c0b50412409c1e4b5b83742babd
inkuniverse <inkuniverse@outlook.com>
2020-09-06 19:35:13 +0800

    lint

--- main.cpp
+++ main.cpp
@@ -1,25 +1,115 @@
-#include<stdio.h>
-int dp[1000010], g1[1000010], g2[1000010], cnt;
-int main()
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define M 40
+#define p 100000000
+#define ll long long
+struct LINT
 {
-	int n, m;
-	scanf("%d%d", &n, &m);
-	for (int i = 0; i < n; ++i)
-	{
-		int v, w, s;
-		scanf("%d%d%d", &v, &w, &s);
-		for (int k = 1; k <= s; k *= 2)
-		{
-			s -= k;
-			g1[cnt] = v * k;
-			g2[cnt++] = w * k;
-		}
-		if (s > 0)g1[cnt] = v * s, g2[cnt++] = w * s;
-	}
-    for(int i = 0;i < cnt;i++)
-        for(int j = m;j >= g1[i]; j--)
-            if(dp[j - g1[i]] + g2[i] > dp[j]) dp[j] = dp[j - g1[i]] + g2[i];
-            
-	printf("%d", dp[m]);
-	return 0;
-}
+    ll x[M];
+    int len;
+    LINT()
+    {
+        memset(x, 0, sizeof x);
+        len = 0;
+    }
+    LINT(const int &x) { *this = x; }
+    inline void operator=(const int &x)
+    {
+        LINT res;
+        res.len = 1;
+        res[0] = x;
+        *this = res;
+    }
+    inline ll &operator[](const int &cur) { return x[cur]; }
+    inline const ll &operator[](const int &cur) const { return x[cur]; }
+    LINT operator+(const LINT &a) const
+    {
+        LINT res;
+        res.len = max(len, a.len) + 1;
+        for (int i = 0; i < res.len; i++)
+        {
+            res[i] += x[i] + a[i];
+            if (res[i] >= p)
+                res[i + 1]++, res[i] -= p;
+        }
+        while (res.len > 1 && res[res.len - 1] == 0)
+            res.len--;
+        return res;
+    }
+    inline void operator+=(const LINT &a) { *this = *this + a; } // ------------------------------------------------------------------------
+    LINT operator-(const LINT &a) const
+    {
+        LINT res;
+        res.len = max(len, a.len) + 1;
+        for (int i = 0; i < res.len; i++)
+        {
+            res[i] += x[i] + a[i];
+            if (res[i] < 0)
+                res[i] += p, res[i + 1]--;
+        }
+        while (res.len > 1 && res[res.len - 1] == 0)
+            res.len--;
+        return res;
+    }
+    inline void operator-=(const LINT &a) { *this = *this - a; }
+    LINT operator*(const int &a) const
+    {
+        LINT res;
+        res.len = len + 1;
+        for (int i = 0; i < len; i++)
+        {
+            res[i] += x[i] * a;
+            if (res[i] > p)
+                res[i + 1] = res[i] / p, res[i] %= p;
+        }
+        while (res.len > 1 && res[res.len - 1] == 0)
+            res.len--;
+        return res;
+    }
+    inline void operator*=(const int &a) { *this = *this * a; }
+    LINT operator/(const int &a) const
+    {
+        LINT res;
+        res.len = len;
+        ll tmp = 0;
+        for (int i = len - 1; ~i; i--)
+        {
+            tmp *= p;
+            res[i] += (x[i] + tmp) / a;
+            tmp = (x[i] + tmp) % a;
+        }
+        while (res.len > 1 && res[res.len - 1] == 0)
+            res.len--;
+        return res;
+    }
+    inline void operator/=(const int &a) { *this = *this / a; } // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    inline bool operator<(const LINT &a) const
+    {
+        if (len != a.len)
+            return len < a.len;
+        for (int i = len - 1; ~i; i--)
+            if (x[i] != a[i])
+                return x[i] < a[i];
+        return false;
+    }
+    inline bool operator==(const LINT &a) const { return !(*this < a) && !(a < *this); }
+    inline bool operator<=(const LINT &a) const { return *this == a || *this < a; }
+    inline bool operator>(const LINT &a) const { return a < *this; }
+    inline bool operator>=(const LINT &a) const { return a == *this || a < *this; } // Func
+    inline void print()
+    {
+        int i;
+        for (i = len - 1; ~i; i--)
+            if (x[i] || i == 0)
+            {
+                printf("%lld", x[i]);
+                break;
+            }
+        for (i = i - 1; ~i; i--)
+            printf("%08lld", x[i]);
+    }
+};
+int main() {}
