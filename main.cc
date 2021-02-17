3f0924b2a2812381b6b63c7c284b1a5ec8779573
inkuniverse <inkuniverse@outlook.com>
2021-02-17 02:30:13 +0000

    hah

--- main.cc
+++ main.cc
@@ -1,22 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 5e5 + 10;
-int T, n;
-long long a[N];
-int main(void)
+
+// 线性基 (p)
+int p[1000010], n;
+int calcmax()
 {
-    scanf("%d", &T);
-    while(T--)
+    sort(p + 1, p + n + 1, greater<int>());
+    int v = 0;
+    for(int i = 1; i <= n; i++)
+        v = max(v, v ^ p[i]);
+    return v;
+}
+
+int calckth(int k) // k 小
+{
+    sort(p + 1, p + n + 1, greater<int>());
+    k += 1; // 否则会算入0
+    int v = 0;
+    for(int i = 1; i <= n; i++)
     {
-        scanf("%d", &n);
-        for(int i = 1; i <= n; i++)
-            scanf("%lld", a + i);
-        long long ans = 0;
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= n; j++)
-                for(int k = 1; k <= n; k++)
-                    ans = (ans + (((a[i] & a[j]) % (long long)(1000000007)) * (a[j] | a[k])) % (long long)(1000000007)) % (long long)(1000000007);
-        
-        printf("%lld\n", ans);
+        int v1 = v ^ p[i];
+        if(v > v1) swap(v, v1);
+        if(k > 1 << (n - i))
+            v = v1, k -= 1 << (n - i);
+    }
+    return v;
+}
+
+int calc(int x) // 计算最高位
+{
+    int i = 0;
+    for(;x;++i, x /= 2);
+    return i;
+}
+int add(int x)
+{
+    for(int i = n; i; --i)
+        if(calc(p[i]) == calc(x))
+            x ^= p[i];
+    if(x)
+    {
+        p[++n] = x;
+        sort(p + 1, p + n + 1);
     }
 }
